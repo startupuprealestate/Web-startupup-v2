7997dd1e0ae0b004266fbf611ee068d699eb554d
Startup Up <startup.up.real.estate@gmail.com>
2026-06-23 15:36:08 +0700

    Google Tag v.2

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,6 @@
 import 'leaflet/dist/leaflet.css'
 import '../styles/globals.css'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-WDZMN7ZZGQ"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-WDZMN7ZZGQ');
-        `}
-      </Script>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
