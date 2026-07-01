cc519277c0a4bfd0b7f55e95a094f5fedca7b43e
Startup Up <startup.up.real.estate@gmail.com>
2026-07-01 11:17:27 +0700

    Ads Google

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,20 @@ import '../styles/globals.css'
 import Script from 'next/script'
 
 const GOOGLE_TAG_ID = 'G-989XMRNC6Y'
+const GOOGLE_TAG_MANAGER_ID = 'GTM-N27PQGL2'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');
+        `}
+      </Script>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`}
         strategy="afterInteractive"
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const GOOGLE_TAG_MANAGER_ID = 'GTM-N27PQGL2'
+
 export default function Document() {
   return (
     <Html lang="th">
@@ -9,6 +11,14 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
